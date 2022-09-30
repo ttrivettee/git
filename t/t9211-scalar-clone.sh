c1c7e2f049e762b9b60614a5732e4d41db1d0da5
Derrick Stolee <derrickstolee@github.com>
2022-09-30 12:44:27 -0400

    scalar: add --[no-]src option
    Some users have strong aversions to Scalar's opinion that the repository
    should be in a 'src' directory, even though it creates a clean slate for
    placing build outputs in adjacent directories.
    
    The --no-src option allows users to opt-out of the default behavior.
    
    While adding options, make sure the usage output by 'scalar clone -h'
    reports the same as the SYNOPSIS line in Documentation/scalar.txt.
    
    Signed-off-by: Derrick Stolee <derrickstolee@github.com>

--- t/t9211-scalar-clone.sh
+++ t/t9211-scalar-clone.sh
@@ -180,4 +180,12 @@ test_expect_success 'scalar clone warns when background maintenance fails' '
 	grep "could not turn on maintenance" err
 '
 
+test_expect_success '`scalar clone --no-src`' '
+	scalar clone --src "file://$(pwd)/to-clone" with-src &&
+	scalar clone --no-src "file://$(pwd)/to-clone" without-src &&
+
+	test_path_is_dir with-src/src &&
+	test_path_is_missing without-src/src
+'
+
 test_done
