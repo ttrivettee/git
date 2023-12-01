19233aa0d4496b66d67fbee82fb8d9b6b35a03cb
John Cai <johncai86@gmail.com>
2023-12-01 15:23:20 -0500

    t1414: convert test to use Git commands instead of writing refs manually
    This test can be re-written to use Git commands rather than writing a
    manual ref in the reflog. This way this test no longer needs the
    REFFILES prerequisite.
    
    Signed-off-by: John Cai <johncai86@gmail.com>

--- t/t1414-reflog-walk.sh
+++ t/t1414-reflog-walk.sh
@@ -121,13 +121,14 @@ test_expect_success 'min/max age uses entry date to limit' '
 
 # Create a situation where the reflog and ref database disagree about the latest
 # state of HEAD.
-test_expect_success REFFILES 'walk prefers reflog to ref tip' '
+test_expect_success 'walk prefers reflog to ref tip' '
+	test_commit A &&
+	test_commit B &&
+	git reflog delete HEAD@{0} &&
 	head=$(git rev-parse HEAD) &&
-	one=$(git rev-parse one) &&
-	ident="$GIT_COMMITTER_NAME <$GIT_COMMITTER_EMAIL> $GIT_COMMITTER_DATE" &&
-	echo "$head $one $ident	broken reflog entry" >>.git/logs/HEAD &&
+	A=$(git rev-parse A) &&
 
-	echo $one >expect &&
+	echo $A >expect &&
 	git log -g --format=%H -1 >actual &&
 	test_cmp expect actual
 '
