273415aa6a45ac7e5151f7c13a2676eed6c88eac
Chandra Pratap <chandrapratap3519@gmail.com>
2023-12-27 12:06:41 +0530

    sideband.c: remove redundant 'NEEDSWORK' tag
    Signed-off-by: Chandra Pratap <chandrapratap3519@gmail.com>

--- sideband.c
+++ sideband.c
@@ -69,7 +69,8 @@ void list_config_color_sideband_slots(struct string_list *list, const char *pref
  * of the line. This should be called for a single line only, which is
  * passed as the first N characters of the SRC array.
  *
- * NEEDSWORK: use "size_t n" instead for clarity.
+ * It is fine to use "int n" here instead of "size_t n" as all calls to this
+ * function pass an 'int' parameter.
  */
 static void maybe_colorize_sideband(struct strbuf *dest, const char *src, int n)
 {
