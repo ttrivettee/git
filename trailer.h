ed67ebf8647bf7ed7d3effb65b4759cde0ebf759
Linus Arver <linusa@google.com>
2023-12-30 06:59:38 -0800

    trailer: delete obsolete argument handling code from API
    This commit was not squashed with its parent in order to keep the diff
    separate (to make the additive changes in the parent easier to read).
    
    Signed-off-by: Linus Arver <linusa@google.com>

--- trailer.h
+++ trailer.h
@@ -32,20 +32,6 @@ int trailer_set_where(enum trailer_where *item, const char *value);
 int trailer_set_if_exists(enum trailer_if_exists *item, const char *value);
 int trailer_set_if_missing(enum trailer_if_missing *item, const char *value);
 
-/*
- * A list that represents newly-added trailers, such as those provided
- * with the --trailer command line option of git-interpret-trailers.
- */
-struct new_trailer_item {
-	struct list_head list;
-
-	const char *text;
-
-	enum trailer_where where;
-	enum trailer_if_exists if_exists;
-	enum trailer_if_missing if_missing;
-};
-
 void trailer_conf_set(enum trailer_where where,
 		      enum trailer_if_exists if_exists,
 		      enum trailer_if_missing if_missing,
@@ -78,9 +64,6 @@ struct process_trailer_options {
 #define PROCESS_TRAILER_OPTIONS_INIT {0}
 
 void parse_trailers_from_config(struct list_head *config_head);
-
-void parse_trailers_from_command_line_args(struct list_head *arg_head,
-					   struct list_head *new_trailer_head);
 
 void process_trailers_lists(struct list_head *head,
 			    struct list_head *arg_head);
