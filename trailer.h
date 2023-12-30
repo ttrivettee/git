94bf182e3ffbf8ed6e20cd77b2e46e5b83c44d34
Linus Arver <linusa@google.com>
2023-12-30 07:12:25 -0800

    trailer: prepare to delete "parse_trailers_from_command_line_args()"
    Expose more functions in the trailer.h API, in preparation for deleting
    
        parse_trailers_from_command_line_args()
    
    from the trailers implementation, because it should not be concerned
    with command line arguments (as they have nothing to do with trailers
    themselves). Indeed, the interpret-trailers builtin is the only caller
    of this function inside Git.
    
    Rename add_arg_item() to trailer_add_arg_item() to expose it as an API
    function. Rename new_trailers_clear() to free_new_trailers() because it
    will be promoted into an API function; the API already has
    free_trailers(), so using the "free_*" naming style will keep it
    consistent. Also rename "conf_info" to "trailer_conf" for readability,
    dropping the low-value "_info" suffix as we did earlier in this series
    for "trailer_info" to "trailer_block".
    
    Helped-by: Josh Steadmon <steadmon@google.com>
    Signed-off-by: Linus Arver <linusa@google.com>

--- trailer.h
+++ trailer.h
@@ -5,6 +5,7 @@
 #include "strbuf.h"
 
 struct trailer_block;
+struct trailer_conf;
 
 enum trailer_where {
 	WHERE_DEFAULT,
@@ -45,6 +46,12 @@ struct new_trailer_item {
 	enum trailer_if_missing if_missing;
 };
 
+void duplicate_trailer_conf(struct trailer_conf *dst,
+			    const struct trailer_conf *src);
+void trailer_add_arg_item(struct list_head *arg_head, char *tok, char *val,
+			  const struct trailer_conf *conf,
+			  const struct new_trailer_item *new_trailer_item);
+
 struct process_trailer_options {
 	int in_place;
 	int trim_empty;
@@ -69,6 +76,12 @@ void parse_trailers_from_command_line_args(struct list_head *arg_head,
 
 void process_trailers_lists(struct list_head *head,
 			    struct list_head *arg_head);
+
+ssize_t find_separator(const char *line, const char *separators);
+
+void parse_trailer(const char *line, ssize_t separator_pos,
+		   struct strbuf *tok, struct strbuf *val,
+		   const struct trailer_conf **);
 
 struct trailer_block *parse_trailers(const struct process_trailer_options *,
 				     const char *str,
