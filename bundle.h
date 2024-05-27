0a18d7839be67d6c0be137c7e15dff9663a161a8
Xing Xin <xingxin.xx@bytedance.com>
2024-05-27 11:39:21 +0800

    unbundle: extend options to support object verification
    This commit extends object verification support in `bundle.c:unbundle`
    by adding two new options to `verify_bundle_flags`:
    
    - `VERIFY_BUNDLE_FSCK_ALWAYS` explicitly enables checks for broken
      objects. It will be used to add "--fsck-objects" support for "git
      bundle unbundle" in a separate series.
    - `VERIFY_BUNDLE_FSCK_FOLLOW_FETCH` is designed to be used during fetch
      operations, specifically for direct bundle fetches and _bundle-uri_
      enabled fetches. When enabled, `bundle.c:unbundle` invokes
      `fetch-pack.c:fetch_pack_fsck_objects` to determine whether to enable
      checks for broken objects. Passing this flag during fetching will be
      implemented in a subsequent commit.
    
    Note that the option `VERIFY_BUNDLE_FSCK_ALWAYS` takes precedence over
    `VERIFY_BUNDLE_FSCK_FOLLOW_FETCH`.
    
    Reviewed-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Xing Xin <xingxin.xx@bytedance.com>

--- bundle.h
+++ bundle.h
@@ -33,6 +33,8 @@ int create_bundle(struct repository *r, const char *path,
 enum verify_bundle_flags {
 	VERIFY_BUNDLE_VERBOSE = (1 << 0),
 	VERIFY_BUNDLE_QUIET = (1 << 1),
+	VERIFY_BUNDLE_FSCK_ALWAYS = (1 << 2),
+	VERIFY_BUNDLE_FSCK_FOLLOW_FETCH = (1 << 3),
 };
 
 int verify_bundle(struct repository *r, struct bundle_header *header,
