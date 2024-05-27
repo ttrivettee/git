2f15099bbb9fed4919e829274b7683ace9094d15
Xing Xin <xingxin.xx@bytedance.com>
2024-05-27 11:39:21 +0800

    unbundle: extend object verification for fetches
    The existing fetch.fsckObjects and transfer.fsckObjects configurations
    were not fully applied to bundle-involved fetches, including direct
    bundle fetches and bundle-uri enabled fetches. Furthermore, there was no
    object verification support for unbundle.
    
    This commit extends object verification support in `bundle.c:unbundle`
    by adding the `VERIFY_BUNDLE_FSCK` option to `verify_bundle_flags`. When
    this option is enabled, we append the `--fsck-objects` flag to
    `git-index-pack`.
    
    The `VERIFY_BUNDLE_FSCK` option is now used by bundle-involved fetches,
    where we use `fetch-pack.c:fetch_pack_fsck_objects` to determine whether
    to enable this option for `bundle.c:unbundle`, specifically in:
    
    - `transport.c:fetch_refs_from_bundle` for direct bundle fetches.
    - `bundle-uri.c:unbundle_from_file` for bundle-uri enabled fetches.
    
    This addition ensures a consistent logic for object verification during
    fetches. Tests have been added to confirm functionality in the scenarios
    mentioned above.
    
    Reviewed-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Xing Xin <xingxin.xx@bytedance.com>

--- t/t5558-clone-bundle-uri.sh
+++ t/t5558-clone-bundle-uri.sh
@@ -34,7 +34,21 @@ test_expect_success 'create bundle' '
 		sed -e "/^$/q" -e "s/$(git rev-parse A) /$(git rev-parse B) /" \
 			<A.bundle >bad-header.bundle &&
 		convert_bundle_to_pack \
-			<A.bundle >>bad-header.bundle
+			<A.bundle >>bad-header.bundle &&
+
+		cat >data <<-EOF &&
+		tree $(git rev-parse HEAD^{tree})
+		parent $(git rev-parse HEAD)
+		author A U Thor
+		committer A U Thor
+
+		commit: this is a commit with bad emails
+
+		EOF
+		git hash-object --literally -t commit -w --stdin <data >commit &&
+		git branch bad $(cat commit) &&
+		git bundle create bad-object.bundle bad &&
+		git update-ref -d refs/heads/bad
 	)
 '
 
@@ -53,6 +67,23 @@ test_expect_success 'clone with bundle that has bad header' '
 	commit_b=$(git -C clone-from rev-parse B) &&
 	test_grep "trying to write ref '\''refs/bundles/topic'\'' with nonexistent object $commit_b" err &&
 	git -C clone-bad-header for-each-ref --format="%(refname)" >refs &&
+	! grep "refs/bundles/" refs
+'
+
+test_expect_success 'clone with bundle that has bad object' '
+	# Unbundle succeeds if no fsckObjects confugured.
+	git clone --bundle-uri="clone-from/bad-object.bundle" \
+		clone-from clone-bad-object-no-fsck &&
+	git -C clone-bad-object-no-fsck for-each-ref --format="%(refname)" >refs &&
+	grep "refs/bundles/" refs >actual &&
+	test_write_lines refs/bundles/bad >expect &&
+	test_cmp expect actual &&
+
+	# Unbundle fails with fsckObjects set true, but clone can still proceed.
+	git -c fetch.fsckObjects=true clone --bundle-uri="clone-from/bad-object.bundle" \
+		clone-from clone-bad-object-fsck 2>err &&
+	test_grep "missingEmail" err &&
+	git -C clone-bad-object-fsck for-each-ref --format="%(refname)" >refs &&
 	! grep "refs/bundles/" refs
 '
 
