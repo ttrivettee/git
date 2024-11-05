8d7eea4af5b8b3c22b25776ff01a24fb1d9fb4f0
Taylor Blau <me@ttaylorr.com>
2024-11-05 14:05:17 -0500

    t/helper/test-tool: implement sha1-unsafe helper
    Add a new helper similar to 't/helper/test-tool sha1' called instead
    "sha1-unsafe" which uses the unsafe variant of Git's SHA-1 wrappers.
    
    While we're at it, modify the test-sha1.sh script to exercise both
    the sha1 and sha1-unsafe test tools to ensure that both produce the
    expected hash values.
    
    Signed-off-by: Taylor Blau <me@ttaylorr.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/helper/test-sha1.c
+++ t/helper/test-sha1.c
@@ -13,3 +13,8 @@ int cmd__sha1_is_sha1dc(int argc UNUSED, const char **argv UNUSED)
 #endif
 	return 1;
 }
+
+int cmd__sha1_unsafe(int ac, const char **av)
+{
+	return cmd_hash_impl(ac, av, GIT_HASH_SHA1, 1);
+}
